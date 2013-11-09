f5c3a7c208cfda3148bd267cf210b3fe1a2f26e6
asashinoda <asashinoda@gmail.com>
2013-11-09 02:57:25 +0000

    Rails init

---
+++ .bash_history
@@ -0,0 +1,11 @@
+mkdir railsgirls
+cd
+cd railsgirls
+rails new asa
+cd asa
+cd app
+ls -al
+cd ..
+cd ....
+cd.. cd.. 
+rails server
---
+++ railsgirls/asa/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Asa::Application.config.secret_key_base = 'e80997479b9c3b296c2f5f5f5d5d26f902f57a2b16b152297ce0c6ffb64d81e1f17f2b92ee250805114df1591f82aab501502aa08b7a91f72656b8824e03610e'
